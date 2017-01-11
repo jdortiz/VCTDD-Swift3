2a3cd5554bccd32ca2ce57addbea647da04dea49
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2017-01-11 12:46:17 +0100

    Basic test working.

--- VCTDD/GeniusesTableViewController.swift
+++ VCTDD/GeniusesTableViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class GeniusesTableViewController: UITableViewController {
 
+    // MARK: - Parameters & Constants
+
+    static let ID = "GeniusesTableViewController"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
